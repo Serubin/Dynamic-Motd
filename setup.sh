26862e2e94d73c208846c44b3cf1244a09503ff6
Serubin <Serubin@Serubin.net>
2016-04-01 12:19:34 -0400

    Simple dynamic motd script for debian.

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "This script requires root access. I recommend you look at what this script does before giving it access"
+
+sudo echo -e ""
+
+# Copy over scripts
+sudo cp -r update-motd.d /etc/
+
+# Set correct permissions
+sudo chmod 755 /etc/update-motd.d
+sudo chmod +x /etc/update-motd.d/*
+
+# Update motd
+sudo rm /etc/motd
+sudo ln -s /var/run/motd /etc/motd
+
+echo "Dynamic motd is now installed."
+echo "Make sure to disable 'printMotd' in the sshd config and to enable 'printLastLog'"
+
